24563f4449aa2efdf4c35fff1d7a157509250de1
Yashwanth Yalavali <yalavali.venkatasubbaiah22@st.niituniversity.in>
2025-04-24 16:39:17 +0530

    Modified Server.js file

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Node.js container!');
+  <h1>Hello from the Node.js container!</h1>
+  res.send(' YO YO HONEY SINGH ');
 });
 
 app.listen(port, () => {
